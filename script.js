84b143ffa496beabf3b559af64a14ec5ad1821d9
Whybootai <info.editorush@gmail.com>
2025-01-27 15:35:50 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function toggleModal() {
-    const modal = document.getElementById('authModal');
-    const backdrop = document.getElementById('backdrop');
-    modal.classList.toggle('open');
-    backdrop.classList.toggle('open');
-}
